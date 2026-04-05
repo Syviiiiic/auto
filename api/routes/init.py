ae183856be49a83f76b9af9dde652055ae939f2c
Syviiiiic <125569515+Syviiiiic@users.noreply.github.com>
2026-04-05 18:16:25 +0300

    Update init.py

--- api/routes/init.py
+++ api/routes/init.py
@@ -1,8 +1,1 @@
-from . import auth
-from . import ads
-from . import search
-from . import uploads
-from . import favorites
-from . import users
-
-__all__ = ["auth", "ads", "search", "uploads", "favorites", "users"]
+from . import ads, auth, favorites, search, uploads, users
