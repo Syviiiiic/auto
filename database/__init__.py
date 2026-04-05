f19671cbac67ad9d95cc21b158f5d9962d874318
Syviiiiic <125569515+Syviiiiic@users.noreply.github.com>
2026-04-05 19:17:44 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,14 +1,2 @@
-from .db import init_db, get_db
-from .models import User, Ad, Favorite
-from .queries import UserQueries, AdQueries, FavoriteQueries
-
-__all__ = [
-    'init_db',
-    'get_db',
-    'User',
-    'Ad',
-    'Favorite',
-    'UserQueries',
-    'AdQueries',
-    'FavoriteQueries'
-]
+from .db import get_db, init_db, engine
+from .models import Base
